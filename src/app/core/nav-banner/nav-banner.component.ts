4e4271cb5d71f69a10e41d9d74ec21539ca9d1ae
alexrubca <alejandrorubiocalvente@gmail.com>
2018-10-15 11:50:28 +0200

    Add responsive to nav-banner

--- src/app/core/nav-banner/nav-banner.component.ts
+++ src/app/core/nav-banner/nav-banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-banner',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-banner.component.sass']
 })
 export class NavBannerComponent implements OnInit {
+  public show: boolean;
+  public home: boolean;
+  public employee: boolean;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    if (sessionStorage.getItem('active')) {
+      const active = sessionStorage.getItem('active');
+      if (active === 'home') {
+        this.home = true;
+      } else {
+        this.employee = true;
+      }
+    } else {
+      this.home = true;
+      sessionStorage.setItem('active', 'home');
+    }
   }
 
+  /**
+   * activeItem
+   */
+  public activeItem() {
+    this.home = !this.home;
+    this.employee = !this.employee;
+
+    if (this.home) {
+      sessionStorage.setItem('active', 'home');
+    } else {
+      sessionStorage.setItem('active', 'employee');
+    }
+  }
 }
